fdff758ece40e3ba46d348dc5d7a12005502589f
Kateryn Guarcas <26006042@galileo.edu>
2026-03-16 20:04:34 -0600

    codigo para mostrar HOLA MUNDO y la imagen

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+// Definimos el nombre en una variable
+$nombre = "Kateryn Guarcas";
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Mi proyecto de software</title>
+        <Style>
+            body {
+                background-color: #1a1a2e;
+                /*color de fondo oscuro*\
+            color: #e94560; /*color de letra llamativo *\
+                font-family: 'segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+                display: flex;
+                flexdirection: colum;
+                align-items: center;
+                justify-content:center;
+                height: 100vh;
+                margin: 0;
+}
+                h1 {
+                font-size: 3rem;
+                text-shadow: 2px 2px #0f3460;
+                margin-bottom: 20px;
+}
+                .container {
+                text-aling: center;
+                padding> 20px;
+                border: 2px solid #16213e;
+                box-shadow: 0 0 20px rgba(233, 69, 96, 0.5);
+}
+                img{
+                width: 400px;
+                border-radius: 10px;
+                margin-top: 20px;
+                border: 3px solid #0f3460;
+}
+                </style>
+                </head>
+                <body>
+
+                <div class="container">
+                <h1>Hola Mundo!</h1>
+                <p style="font-size: 1.5rem;">Soy <strong><?php echo $nombre;
+                ?></strong></p>
+
+                <img src="entrando_al_mundo_digital.jpg"
+                alt="Imagen generada por Leonardo AI">
+                </div>
+
+                </body>
+                </html>
+            
